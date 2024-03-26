e6a86c8521a78e50a9fc7beb585897974797e10b
Guillermina <guillecarrascosuarez@gmail.com>
2024-03-26 13:48:24 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-// using namespace std
 
 
-void main()
+int main()
 {
-    printf("hola mundo");
+    int numero=11,
+    *pnumero;
+    pnumero = &numero;
+    printf("El contenido de lo que apunta el puntero: %d\n",*pnumero);
+    printf("La direccion de memoria almacenada por el puntero: %p\n", pnumero); 
+    printf("La direccion de memoria de la vble: %p\n", &numero);
+    printf("La direccion de memoria del puntero: %p\n", &pnumero);
+    printf("Tamaño de memoria utilizado por esa variable usando la función sizeof(): %d bytes\n", sizeof(numero));
+return 0;
 }
